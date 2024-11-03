b68881aaa4391dc3e1391d3edfc02f81f7ca5ec9
Arthur Battut <a.b@ttut.fr>
2024-11-03 21:33:59 +0400

    Add about page and html templating

---
+++ main.ts
@@ -0,0 +1,40 @@
+import { Application } from "jsr:@oak/oak/application";
+import { Router } from "jsr:@oak/oak/router";
+
+const HtmlTemplate = await Deno.readTextFile("./templates/index.html");
+
+const HomePage = await Deno.readTextFile("./pages/home.html");
+const AboutPage = await Deno.readTextFile("./pages/about.html");
+
+const TemplateContentInsertion = "{ content }";
+
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.headers = new Headers({
+    "Content-Type": "text/html",
+    "X-Frame-Options": "DENY",
+    "X-Content-Type-Options": "nosniff",
+  });
+
+  ctx.response.body = HtmlTemplate.replace(TemplateContentInsertion, HomePage);
+});
+router.get("/about", (ctx) => {
+  ctx.response.headers = new Headers({
+    "Content-Type": "text/html",
+    "X-Frame-Options": "DENY",
+    "X-Content-Type-Options": "nosniff",
+  });
+
+  ctx.response.body = HtmlTemplate.replace(TemplateContentInsertion, AboutPage);
+});
+
+const app = new Application();
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  console.log(`${ctx.request.method} ${ctx.request.url}`);
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 80 });
