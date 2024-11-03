f6b49ea0ca8f1f27df900ec9b70b6a81ba487a66
Arthur Battut <a.b@ttut.fr>
2024-11-03 21:40:08 +0400

    Add rss feed

--- main.ts
+++ main.ts
@@ -6,27 +6,45 @@ const HtmlTemplate = await Deno.readTextFile("./templates/index.html");
 const HomePage = await Deno.readTextFile("./pages/home.html");
 const AboutPage = await Deno.readTextFile("./pages/about.html");
 
+const rssFeedFile = await Deno.readTextFile("./feed.xml");
+
 const TemplateContentInsertion = "{ content }";
 
 const router = new Router();
-router.get("/", (ctx) => {
-  ctx.response.headers = new Headers({
-    "Content-Type": "text/html",
-    "X-Frame-Options": "DENY",
-    "X-Content-Type-Options": "nosniff",
-  });
+router
+  .get("/", (ctx) => {
+    ctx.response.headers = new Headers({
+      "Content-Type": "text/html",
+      "X-Frame-Options": "DENY",
+      "X-Content-Type-Options": "nosniff",
+    });
 
-  ctx.response.body = HtmlTemplate.replace(TemplateContentInsertion, HomePage);
-});
-router.get("/about", (ctx) => {
-  ctx.response.headers = new Headers({
-    "Content-Type": "text/html",
-    "X-Frame-Options": "DENY",
-    "X-Content-Type-Options": "nosniff",
-  });
+    ctx.response.body = HtmlTemplate.replace(
+      TemplateContentInsertion,
+      HomePage,
+    );
+  })
+  .get("/about", (ctx) => {
+    ctx.response.headers = new Headers({
+      "Content-Type": "text/html",
+      "X-Frame-Options": "DENY",
+      "X-Content-Type-Options": "nosniff",
+    });
 
-  ctx.response.body = HtmlTemplate.replace(TemplateContentInsertion, AboutPage);
-});
+    ctx.response.body = HtmlTemplate.replace(
+      TemplateContentInsertion,
+      AboutPage,
+    );
+  })
+  .get("/feed", (ctx) => {
+    ctx.response.headers = new Headers({
+      "Content-Type": "application/xml",
+      "X-Frame-Options": "DENY",
+      "X-Content-Type-Options": "nosniff",
+    });
+
+    ctx.response.body = rssFeedFile;
+  });
 
 const app = new Application();
 // Logger
